8250a667e98fd6082eff10e6eedb1067948982e8
Noratho <60336216+Noratho@users.noreply.github.com>
2022-08-02 23:51:45 -0400

    Reload config

--- src/main/java/me/timelesspvp/timelesspvp5/commands/reloadConfig.java
+++ src/main/java/me/timelesspvp/timelesspvp5/commands/reloadConfig.java
@@ -1,17 +1,30 @@
 package me.timelesspvp.timelesspvp5.commands;
 
+import me.timelesspvp.timelesspvp5.TimelessPvP5;
+import org.bukkit.Bukkit;
+import org.bukkit.ChatColor;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
 import org.jetbrains.annotations.NotNull;
 
-public class reloadKit implements CommandExecutor {
+public class reloadConfig implements CommandExecutor {
 
     @Override
     public boolean onCommand(@NotNull CommandSender sender,
                              @NotNull Command command,
                              @NotNull String label,
                              @NotNull String[] args) {
+
+        TimelessPvP5.getPlugin().reloadConfig();
+
+        if (sender instanceof Player) {
+            Player p = (Player) sender;
+            p.sendMessage(ChatColor.BLUE + "Plugin Config Reloaded");
+        }
+        Bukkit.getLogger().info("reload configssss");
+
         return true;
     }
 }
