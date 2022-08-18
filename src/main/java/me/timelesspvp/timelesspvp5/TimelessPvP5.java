d434282a561650f449abe4c06c2314d5400083e5
Noratho <60336216+Noratho@users.noreply.github.com>
2022-08-18 15:11:59 -0400

    LivingEntityData manager added to plugin main

--- src/main/java/me/timelesspvp/timelesspvp5/TimelessPvP5.java
+++ src/main/java/me/timelesspvp/timelesspvp5/TimelessPvP5.java
@@ -3,6 +3,7 @@ package me.timelesspvp.timelesspvp5;
 import me.timelesspvp.timelesspvp5.commands.leaveArena;
 import me.timelesspvp.timelesspvp5.commands.lobbyCommand;
 import me.timelesspvp.timelesspvp5.commands.reloadConfig;
+import me.timelesspvp.timelesspvp5.dataClasses.LivingEntityData;
 import me.timelesspvp.timelesspvp5.dataClasses.PlayerData;
 import me.timelesspvp.timelesspvp5.listeners.*;
 import org.bukkit.Bukkit;
@@ -10,6 +11,7 @@ import org.bukkit.GameMode;
 import org.bukkit.Location;
 import org.bukkit.NamespacedKey;
 import org.bukkit.configuration.file.FileConfiguration;
+import org.bukkit.entity.LivingEntity;
 import org.bukkit.entity.Player;
 import org.bukkit.inventory.Inventory;
 import org.bukkit.persistence.PersistentDataContainer;
@@ -28,12 +30,15 @@ public final class TimelessPvP5 extends JavaPlugin {
 
     private static TimelessPvP5 plugin;
     private static Map<UUID, PlayerData> plrData = new HashMap<UUID, PlayerData>();
+    private static Map<UUID, LivingEntityData> liveEntData = new HashMap<UUID, LivingEntityData>();
     private static Map<Pair<UUID, String>, Long> cooldowns = new HashMap<>();
 
 
+    // PlayerData manger methods
     public static Map<UUID, PlayerData> getPlrData() {
         return plrData;
     }
+    public static PlayerData getPlr(UUID uuid) { return plrData.get(uuid); }
     public static void addPlrDataEntry(UUID uuid, Inventory inv, Location loc, GameMode gm) {
         PlayerData data = new PlayerData(uuid, inv, loc, gm);
         TimelessPvP5.plrData.put(uuid, data);
@@ -43,6 +48,21 @@ public final class TimelessPvP5 extends JavaPlugin {
     }
 
 
+    // LivingEntityData manger methods
+    public static Map<UUID, LivingEntityData> getLiveEntData() {
+        return liveEntData;
+    }
+    public static LivingEntityData getEnt(UUID uuid) { return liveEntData.get(uuid); }
+    public static void addLiveEntDataEntry(UUID uuid) {
+        LivingEntityData data = new LivingEntityData(uuid);
+        TimelessPvP5.liveEntData.put(uuid, data);
+    }
+    public static void removeLiveEntDataEntry(UUID uuid) {
+        TimelessPvP5.liveEntData.remove(uuid);
+    }
+
+
+    // Cooldown manger methods
     public static Long getCooldownEntry(UUID uuid, String CDName) {
         return cooldowns.get(new Pair<>(uuid, CDName));
     }
@@ -52,7 +72,6 @@ public final class TimelessPvP5 extends JavaPlugin {
     public static boolean cooldownEntryExists(UUID uuid, String CDName) {
         return cooldowns.containsKey(new Pair<>(uuid, CDName));
     }
-
 
 
     @Override
